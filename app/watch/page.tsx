0f97bb59f22869f1d1ce30d63cfbaf8f4ba014a2
Ahsan Iqbal <revensolutions01@gmail.com>
2024-05-27 21:25:12 +0500

    create watch video page

---
+++ app/watch/page.tsx
@@ -0,0 +1,19 @@
+import { AiOutlineArrowLeft } from "react-icons/ai";
+import { movies } from "../../context/index";
+
+const page = () => {
+  return (
+    <div className="h-screen w-screen bg-black">
+      <nav className="fixed w-full p-4 z-10 flex flex-row items-center gap-8 bg-black bg-opacity-70">
+        <AiOutlineArrowLeft className="text-white" size={40} />
+        <p className="text-white text-1xl md:text-3xl font-bold">
+          <span className="font-light">Watching:</span>
+          {movies[0]?.title}
+        </p>
+      </nav>
+      <video autoPlay controls className="h-full w-full" src={movies[0]?.videoUrl}></video>
+    </div>
+  );
+};
+
+export default page;
