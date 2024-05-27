1c5bcd6af1d81d1e15cf9b710ef8cca63607111d
Ahsan Iqbal <revensolutions01@gmail.com>
2024-05-27 21:23:59 +0500

    create movie list and movie card

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
 import Billboard from "./components/Billboard";
+import MovieList from "./components/MovieList";
 import Navbar from "./components/Navbar";
+import { movies } from "../context/index";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <Billboard />
+      <div className="pb-40">
+        <MovieList data={movies} title="Trending Now" />
+      </div>
     </>
   );
 }
